fe2864e322f8bb39150223310a7b172a7fc12342
Timon Baldow <timon.baldow@gmail.com>
2020-04-15 03:03:33 +0200

    Initial commit

---
+++ DriveFileHelper.cs
@@ -0,0 +1,117 @@
+﻿using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+
+namespace GoogleDriveOrphanFixer
+{
+    class DriveFileHelper
+    {
+        DriveHelper drive;
+        DriveService service;
+
+        public DriveFileHelper(DriveHelper drive)
+        {
+            this.drive = drive;
+            // Create Google Drive Activity API service.
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = drive.Credential,
+                ApplicationName = drive.ApplicationName,
+            });
+        }
+
+        public List<string> ScanForOrphans()
+        {
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = drive.PageSize;
+            listRequest.Fields = "nextPageToken, files(id, name, parents)";
+            FileList response;
+
+            List<string> orphans = new List<string>();
+            int i = 1;
+            do
+            {
+                Console.Write("\rScanning for orphans... Page {0}", i);
+                response = listRequest.Execute();
+                IList<Google.Apis.Drive.v3.Data.File> files = response.Files;
+
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        if (file.Parents == null)
+                        {
+                            orphans.Add("items/" + file.Id);
+                        }
+                    }
+                    catch { Console.WriteLine("Error reading file id from orphan"); }
+                }
+                // check next page
+                listRequest.PageToken = response.NextPageToken;
+            } while (response.NextPageToken != null && response.NextPageToken != "");
+            Console.WriteLine();
+            return orphans;
+        }
+
+        public void MoveOrphanFilesToParent(string globalRestoredFolderId, List<DriveItem> orphansToMove)
+        {
+            for (int i = 0; i < orphansToMove.Count; i++)
+            {
+                Console.Write("\rMoving orphan back into parent folder... {0}/{1}", i+1, orphansToMove.Count);
+                FilesResource.UpdateRequest updateRequest = service.Files.Update(new Google.Apis.Drive.v3.Data.File(), orphansToMove[i].id.Replace("items/", ""));
+                updateRequest.AddParents = orphansToMove[i].parent_id.Replace("items/", "");
+                updateRequest.RemoveParents = globalRestoredFolderId;
+
+                var response = updateRequest.Execute();
+            }
+            Console.WriteLine();
+        }
+
+        public List<string> GetFilesFromFolderById(string parent_id)
+        {
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.Q = String.Format("\"{0}\" in parents", parent_id.Replace("items/", ""));
+            listRequest.Fields = "nextPageToken, files(id, name)";
+            listRequest.PageSize = drive.PageSize;
+
+
+            FileList response;
+
+            List<string> fileIds = new List<string>();
+
+            int i = 1;
+            do
+            {
+                Console.Write("\rFetching files from folder... Page {0}", i);
+                try
+                {
+                    response = listRequest.Execute();
+                    IList<Google.Apis.Drive.v3.Data.File> files = response.Files;
+
+                    if (files != null && files.Count > 0)
+                    {
+                        foreach (var file in files)
+                        {
+                            //Console.WriteLine("{0} ({1})", file.Name, file.Id);
+                            fileIds.Add("items/" + file.Id);
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    listRequest.PageToken = response.NextPageToken;
+                } catch (Google.GoogleApiException e)
+                {
+                    Console.WriteLine("\nERROR: Could not find folder id! " + e.Message);
+                    return fileIds;
+                }
+            } while (response.NextPageToken != null && response.NextPageToken != "");
+            Console.WriteLine();
+            return fileIds;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+## Google Drive Orphan Fixer
+
+Tool for locating orphaned files and folders in Google Drive and restoring their original folder structure.
+
+
+## Purpose
+When sharing files using Google Drive it may happen that a person that has editor permissions deletes a file or folder owned by someone else. *In this case Google Drive will delete the file for the shared person but it won't delete the file for the owner*. Instead, Google Drive marks the file (or folder) as **"unorganized"** - thus the file still exists but lost its parent folder. The file will not show up in Google Drive anymore but still takes up space. If files have been deleted separately from their folder it is almost impossible to restore large numbers of files manually. This is particularly the case when using a Google Drive client, such as the official Backup & Sync. When a person synchronizes a shared folder and later on deletes it directly from the hard drive, the client will not only mark a folder as *unorganized*, but rather each file turns into an orphan.
+
+
+### Am I affected?
+
+There is an easy and quick way to tell: simply open Google Drive and search for *"is:unorganized owner:me"*. Or use the following link: https://drive.google.com/drive/search?q=is:unorganized%20owner:me.
+If any items show up, you either may want to delete them in case you don't need them anymore (since they are *not* in the trash, they still count towards your total used space), or you may wish to restore them to their original path - which is the purpose of this tool.
+
+## Usage
+On the first start of the tool, your browser will open requiring your to login and allow the tool to access your Google Drive data as well its activity. The activity is required to determine from where a folder was moved into the *nirvana*.
+Two modi are supported:
+1. Restore folder structure from files already moved from the *nirvana* into one, large folder
+	* This is the case when you have already manually moved some, or all files into another folder in your Drive
+	* See the next paragraph for further information
+2. Restore the folder structure directly
+	* This is the most common case, where the tool attempts to restore all files directly
+
+You can run both modi if you have already started to move some files.
+
+### Restoring from a folder
+To restore files from a specific folder to their original folder structure the tool will require you to enter the id of this specific folder. To obtain the id you can simply login into your Google Drive via browser, open the folder and copy the id found in the URL, as in:
+>`https://drive.google.com/drive/folders/{folder-id}`
+
+E.g., suppose the link to the folder is`https://drive.google.com/drive/folders/13BivEqW7pKwdGA1zgrolXQjv--2FG6Ms`, the id of the folder is therefore `13BivEqW7pKwdGA1zgrolXQjv--2FG6Ms`
+
+## Configuring the credentials.json
+If the API did not yet get approved and the maximum amount of users is reached the credentials.json file will stop working. To fix this you can either set up your own OAuth login at the [Google Cloud Platform](https://console.cloud.google.com/apis/) and edit the credentials.json file or use the fast way and get your credentials by using the [Quickstart API](https://console.developers.google.com/henhouse/?pb=%5B%22hh-0%22,%22drive%22,null,%5B%5D,%22https:%2F%2Fdevelopers.google.com%22,null,%5B%5D,null,%22Enable%20the%20Drive%20API%22,1,null,%5B%5D,false,false,null,null,null,null,false,null,false,false,null,null,null,%22DESKTOP%22,null,%22Quickstart%22,true,%22Quickstart%22,null,null,false%5D). The credentials.json file must be placed in the same folder as the executable.
+
+## Compiling
+The code is written in C# with .NET Core 3.1. You can use Visual Studio for compiling. Compiling for Linux is untested and probably needs code adjustments to work.
